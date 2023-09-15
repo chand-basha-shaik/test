e4528f3b506dbe276da9ed477ccaf2ebcc659280
91789 <91789@DESKTOP-V6N51HM>
2023-09-15 15:41:55 +0530

    junit

---
+++ User/src/test/java/com/example/User/JunitExample.java
@@ -0,0 +1,16 @@
+package com.example.User;
+
+import org.junit.jupiter.api.Test;
+
+public class JunitExample {
+@Test
+ public void run() {
+	System.out.println();
+	 System.out.println("successfully created junit");
+	 
+ }
+public static void main(String[] args) {
+	JunitExample j=new JunitExample();
+	j.run();
+}
+}
