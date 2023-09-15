8001c81f82abb83176ee71442c8efe76f5fb40cd
91789 <91789@DESKTOP-V6N51HM>
2023-09-15 15:30:42 +0530

    jh

---
+++ CRUD/src/main/java/com/example/CRUD/entity/Data.java
@@ -0,0 +1,43 @@
+package com.example.CRUD.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Data {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String name;
+	private String email;
+	private String pass;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	
+	
+
+}
